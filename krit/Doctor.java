c4da7fc0411e9fdb9c39900c208abeb86d0c861c
enjiaaaa <161003075+enjiaaaa@users.noreply.github.com>
2024-10-27 23:37:05 +0800

    Create Doctor.java

---
+++ krit/Doctor.java
@@ -0,0 +1,56 @@
+package krit;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+
+public class Doctor {
+    private String doctorID;
+    private List<Appointment> appointments;
+    private MedicalRecord medicalRecord;
+    private Map<String, MedicalRecord>  MedicalRecord;
+    private List<Appointment> availability;
+
+
+    // Constructors
+    public Doctor(String doctorID){
+        this.doctorID = doctorID;
+        this.appointments = new ArrayList<>();
+        this.medicalRecord = new MedicalRecord();
+        this.outcomeRecord = new HashMap<>();
+        this.availability = new ArrayList<>();
+        
+    }
+    public MedicalRecords viewPatientMedicalRecords(String patientID){
+        return MedicalRecord.get(patientID, new MedicalRecord());
+    }
+
+    public boolean updatePatientsMedicalRecords (int patientID, MedicalRecord record){
+
+    } 
+    
+    public List<Appointment> viewPersonalSchedule(){
+        return new ArrayList<>(appointments);
+
+    }
+        
+    public boolean acceptAppointmentRequest (int appointmentID){
+
+    }
+    
+    public boolean declineAppointmentRequest (int appointmentID){
+
+    }
+    
+    public List<Appointment> setAvailability (List<Appointment> slots){
+
+    }
+    
+    public boolean recordAppointmentOutcome (int appointmentID, OutcomeRecord outcome){
+
+    }
+    
+
+}
+

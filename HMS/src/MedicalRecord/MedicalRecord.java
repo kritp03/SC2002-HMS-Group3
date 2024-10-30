18a1715723d505cc173dbc291597229c8d78af0e
enjiaaaa <161003075+enjiaaaa@users.noreply.github.com>
2024-10-30 15:21:07 +0800

    commit initial

--- HMS/src/MedicalRecord/MedicalRecord.java
+++ HMS/src/MedicalRecord/MedicalRecord.java
@@ -6,16 +6,28 @@ import java.util.List;
 
 //needs to include diagnosis, treatment and prescription
 public class MedicalRecord {
+    private String patientID;
     private List<Diagnosis> diagnosis;
     private List<Treatment> treatment;
     private List<Prescription> prescription;
 
 //constructor
-    public MedicalRecord (Diagnosis diagnosis, Treatment treatment, Prescription  prescription)
+    public MedicalRecord(String patientID)
     {
+        this.patientID = patientID;
         this.diagnosis = (List<Diagnosis>) diagnosis;
         this.treatment = (List<Treatment>)treatment;
         this.prescription = (List<Prescription>)prescription;
+    }
+
+
+    public String getPatientID(String patientID){
+        return patientID;
+    }
+
+
+    public MedicalRecord() {
+        //TODO Auto-generated constructor stub
     }
 
 }
--- HMS/src/management/Doctor.java
+++ HMS/src/management/Doctor.java
@@ -16,7 +16,7 @@ public class Doctor extends User{
 
     // Constructors
     public Doctor(String doctorID, String name, String emailId ){
-        super(doctorID, name, Role.DOCTOR, "kritpyy@gmail.com" );
+        super(doctorID, name, Role.DOCTOR, "kritpyy@gmail" );
         this.doctorID = doctorID;
         this.appointments = new ArrayList<>();
         this.patientRecords = new ArrayList<>();
@@ -24,14 +24,26 @@ public class Doctor extends User{
         
     }
     // show prescription, diagnosis and treatment
-    public MedicalRecord viewPatientMedicalRecords(String patientID){
-        return MedicalRecord.get(patientID, new MedicalRecord());
+    public MedicalRecord viewPatientMedicalRecords(String patientID) {
+        for (MedicalRecord record : patientRecords) {
+            if (record.getPatientID(patientID).equals(patientID)) {
+                return record;
+            }
+        }
+        return null; // or throw an exception if not found
     }
 
-    public boolean updatePatientsMedicalRecord (int patientID, MedicalRecord record){
-
-    } 
-    
+    public boolean updatePatientsMedicalRecord(String patientID, MedicalRecord updatedRecord) {
+        for (int i = 0; i < patientRecords.size(); i++) {
+            MedicalRecord record = patientRecords.get(i);
+            if (record.getPatientID(patientID).equals(patientID)) {
+                patientRecords.set(i, updatedRecord);
+                return true;
+            }
+        }
+        return false;
+    }
+        
     public List<Appointment> viewPersonalSchedule(){
         return new ArrayList<>(appointments);
 

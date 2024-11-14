44b72eef131a949ca3acea50e2d949ec269a60b8
kritp03 <75032068+kritp03@users.noreply.github.com>
2024-11-14 12:03:42 +0800

    remove unnecessary files

--- HMS/src/misc_classes/Gender.java
+++ HMS/src/misc_classes/Gender.java
@@ -4,12 +4,5 @@ public enum Gender
 {
     MALE,
     FEMALE,
-    TRANSGENDER,
-    GENDER_NEUTRAL,
-    NON_BINARY,
-    AGENDER,
-    PANGENDER,
-    GENDERQUEER,
-    TWO_SPIRIT,
-    THIRD_GENDER
+    OTHERS
 }
